5818415797ac3b438cc88fa9ee0744d384e39e0f
Anand Angalig <anandangalig@gmail.com>
2016-08-19 12:01:16 -0400

    add .forEach to test individual integers

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
   $("form#userInput").submit(function(event){
     event.preventDefault();
-    var input = parseInt($("#inputNumber").val())
-    var inputArray = []
+    var input = parseInt($("#inputNumber").val());
+    var inputArray = [];
 
     for (var index = 1; index <= input; index++) {
       inputArray.push(index);
     }
+
+    inputArray.forEach(function(currentInteger) {
+      if (currentValue === NaN) {
+        alert ("Please enter a number and try again!")
+      }
+    })
+    console.log(currentInteger);
 
   });
 });
