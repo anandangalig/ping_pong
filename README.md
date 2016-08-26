8fa0af841596ca6eb4b5a4caf37096c1b7f5cdb4
Anand <anandangalig@gmail.com>
2016-08-26 10:48:22 -0400

    will add README and separate Business Logic from UI Logic

---
+++ README.md
@@ -0,0 +1,48 @@
+# _Ping Pong Calculator_
+
+#### _A numbers game that returns Ping, Pong, or Ping-Pong depending on the user input, 8/26/2016 _
+
+#### By _**Anand Angalig**_
+
+## Description
+
+_Enter any number as the PING, and we will return a PONG by counting from 1 to your number with three exceptions:_
+
+* _Numbers divisible by 3 are replaced with "ping"_
+* _Numbers divisible by 5 are replaced with "pong"_
+* _Numbers divisible by 15 are replaced with "ping-pong"_
+
+## Specs:
+
+* _If user enters not-a-number (i.e. letters and/or signs), the program will not work and display a message requesting to enter an actual number._
+  * _input: hello_  
+  * _output: "Please enter a number and try again!"_
+* _If user enters a number, the program will count up to the inputted number while returning Ping, Pong, or Ping-Pong for numbers divisible by 3, 5, or 15 respectively._
+  * _input: 5_
+  * _output: 1,2,Ping,4,Pong_
+* _If user enters a number, the program will count up to the inputted number while returning the in-divisible numbers as it is (e.g. 1, 2, 4, or any numbers that are not divisible by 3, 5, or 15)_
+  * _input: 6_
+  * _output: 1,2,Ping,4,Pong,6_
+
+## Setup/Installation Requirements
+
+* _Go to https://github.com/anandangalig/ping_pong.git_
+* _Download this repository onto your local machine_
+* _Open the files in your favorite text editor to see the code_
+* _Open index.html in your web-browser to play the game_
+
+
+## Known Bugs
+
+_No known bugs reported so far. If you find one, please let me know via email at anandangalig@gmail.com_
+
+
+## Technologies Used
+
+* _Jquery_
+
+### License
+
+*This game is licensed under the MIT License agreement.*
+
+Copyright (c) 2016 **_Anand Angalig_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
+//Back End Logic:
+var calculate = function(index) {
+  if (index % 15 ===0) {
+    $("ul").append("<li>" + "PING-PONG!" + "</li>");
+    $("img").show();
+  } else if (index % 5 ===0) {
+    $("ul").append("<li>" + "PONG!" + "</li>");
+    $("img").show();
+  } else if (index % 3 ===0) {
+    $("ul").append("<li>" + "PING!" + "</li>");
+    $("img").show();
+  } else if (index % 3 !==0 || index % 5 !==0 || index % 15 !==0) {
+    $("ul").append("<li>" + index + "</li>");
+    $("img").show();
+  }
+};
+
+
+//Front End Logic:
 $(document).ready(function(){
   $("form#userInput").submit(function(event){
     event.preventDefault();
     $("ul").html("");
-    var input = parseInt($("#inputNumber").val());
+    $("img").hide();
+
     var inputArray = [];
+    var input = parseInt($("#inputNumber").val());
+    if (isNaN(input)) {
+      $("ul").append("<li>" + "Please enter a number and try again!" + "</li>");
+    };
 
     for (var index = 1; index <= input; index++) {
       inputArray.push(index);
+      calculate(index);
     }
-
-
-    inputArray.forEach(function(currentInteger) {
-      if (currentInteger % 15 ===0) {
-        $("ul").append("<li>" + "PING-PONG!" + "</li>");
-        $("img").show();
-      } else if (currentInteger % 5 ===0) {
-        $("ul").append("<li>" + "PONG!" + "</li>");
-        $("img").show();
-      } else if (currentInteger % 3 ===0) {
-        $("ul").append("<li>" + "PING!" + "</li>");
-        $("img").show();
-      } else if (currentInteger % 3 !==0 || currentInteger % 5 !==0 || currentInteger % 15 !==0) {
-        $("ul").append("<li>" + currentInteger + "</li>");
-        $("img").show();
-      //isNaN(currentInteger) and other itirations of it- DID NOT WORK
-      } else {
-        $("ul").append("<li>" + "Please enter a number and try again!" + "</li>");
-      }
-    })
   });
 });
